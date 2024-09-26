9baee1a6836c06d1e2d8226d7f4a2f38554cdf4c
nikhil19d <nikhil19d@gmail.com>
2024-09-26 12:10:12 +0530

    block1

--- src/components/content/Block1.tsx
+++ src/components/content/Block1.tsx
@@ -1,7 +1,36 @@
 const Block1 = () =>{
   return(
   <div className="h-block1">
-  1
+  <div className="h-block1 border flex flex-col justify-evenly">
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+  </div>
+  <div className="h-block1w w-block1 border border-black relative bottom-rel-b1-b left-rel-b1-l flex flex-col justify-evenly rotate-90">
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+  </div>
+  <div className="w-10/12 h-2/4 border border-black relative bottom-b1-div-b left-b1-div-l">
+  <h1>Your complete platform for the web.</h1>
+  <p>Vercel provides the developer tools and cloud infrastructure to build, scale, and secure a faster, more personalized web.</p>
+  <div>
+    <button className="bg-black text-white flex items-center pr-2 pl-2"><svg className="text-white" data-testid="geist-icon" height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16" ><path fill-rule="evenodd" clip-rule="evenodd" d="M8 1L16 15H0L8 1Z" fill="currentColor"></path></svg>Start Deploying</button>
+    <button>Get a Demo</button>
+  </div>
+  </div>
   </div>
   );
 }
